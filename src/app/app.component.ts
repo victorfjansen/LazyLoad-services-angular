0c1026b4954b7907e5521085081b4b0dc997744e
Victor Jansen <victorfjansen@gmail.com>
2023-01-27 14:45:47 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { LazyInjectService } from './services/lazy-inject.service';
+import { LazyServiceService } from './services/lazy-service.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lazy-service';
+
+  constructor(private lazyInject: LazyInjectService) {}
+
+  async doSomething(): Promise<void> {
+    const lazyService = await this.lazyInject.get<LazyServiceService>(() =>
+      import('./services/lazy-service.service').then(
+        (m) => m.LazyServiceService
+      )
+    );
+
+    lazyService.changeBackground();
+  }
 }
